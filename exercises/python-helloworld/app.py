c9a2d357ed1ba3c43afc998a82620e03d2ada54f
Fatiima-Ezzahra <ou.fatimaezzahra@gmail.com>
2021-07-01 18:22:53 +0100

    add logs

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,18 +1,26 @@
+from flask import json
 from flask import Flask
+import logging
+import time
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info("%s, / endpoint was reached" % time.time())
     return "Hello World!"
 
 @app.route("/status")
 def status():
-    return {"result": "OK - healthy"}
+    response = app.response_class(response=json.dumps({"result": "OK - healthy"}), status=200, mimetype="application/json")
+    app.logger.info("%s, /status endpoint was reached" % time.time())
+    return response
 
 @app.route("/metrics")
 def metrics():
-    data = {"UserCount": 140, "UserCountActive": 23}
-    return {"data": data}
+    response = app.response_class(response=json.dumps({"status": "success", "code":0, "data":{"UserCount": 140, "UserCountActive": 23}}), status=200, mimetype="application/json")
+    app.logger.info("%s, /metrics endpoint was reached" % time.time())
+    return response
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="app.log", level=logging.DEBUG)
     app.run(host='0.0.0.0')
